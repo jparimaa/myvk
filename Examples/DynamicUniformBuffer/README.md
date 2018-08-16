13a26ea5fed32baf702433d501ec026d3349dc36
JP Arimaa <juhapekka.arimaa@gmail.com>
2018-08-16 18:37:14 +0300

    Rename Secondary -> SecondaryCommandBuffer, start DynamicUniformBuffer project

---
+++ Examples/DynamicUniformBuffer/README.md
@@ -0,0 +1,5 @@
+# Example
+
+An example project to show basic the configuration and framework usage.
+
+![example](example.png?raw=true "example")
---
+++ Examples/DynamicUniformBuffer/src/main.cpp
@@ -0,0 +1,7 @@
+#include "DynamicApp.h"
+#include "fw/Execute.h"
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    return fw::runApplication<DynamicApp>();
+}
